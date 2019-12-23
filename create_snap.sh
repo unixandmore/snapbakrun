2286b4567a3a7a208dcc90a34e654fdb317f8292
Raymond Cox <rcox@unixandmore.com>
2019-12-23 14:34:07 -0600

    Figured out the array and modified the lvinfo function. Removed the vginfo, moved the required values into lvinfo

--- create_snap.sh
+++ create_snap.sh
@@ -101,15 +101,22 @@ function create_snap {
 
   lvinfo -f ${FILE_SYSTEM} ${VFLAG} 
   (( VERBOSE  == TRUE )) && print -u 2 "# LVINFO............: ${LVINFO[@]}"
-  (( VERBOSE  == TRUE )) && print -u 2 "# LVINFO............: ${LVINFO[${FILE_SYSTEM}].lv_pps}"
+  (( VERBOSE  == TRUE )) && print -u 2 "# LVINFO[lv_pps]....: ${LVINFO[lv_pps]}"
 
   ### .Make sure there is enough space to create the snap
   
-  (( VERBOSE  == TRUE )) && print -u 2 "# LVINFO[FILE_SYSTEM].lv_snap_mb............: ${LVINFO[${FILE_SYSTEM}].lv_snap_mb}"
-  (( VERBOSE  == TRUE )) && print -u 2 "# LVINFO[FILE_SYSTEM].lv_vg_pp_size............: ${LVINFO[${FILE_SYSTEM}].lv_vg_pp_size}"
-  (( SNAP_PP = ${LVINFO[${FILE_SYSTEM}].lv_snap_mb} / ${LVINFO[${FILE_SYSTEM}].lv_vg_pp_size} + 1 ))
+  (( VERBOSE  == TRUE )) && print -u 2 "# LVINFO[lv_snap_mb]...........: ${LVINFO[lv_snap_mb]}"
+  (( VERBOSE  == TRUE )) && print -u 2 "# LVINFO[lv_vg_pp_size]...........: ${LVINFO[lv_vg_pp_size]}"
+  (( SNAP_PP = ${LVINFO[lv_snap_mb]} / ${LVINFO[lv_vg_pp_size]} + 1 ))
   (( VERBOSE  == TRUE )) && print -u 2 "# SNAP_PP.......: ${SNAP_PP}"
 
+  if [[ ${SNAP_PP} -lt ${LVINFO[lv_vg_pp_free]} ]]
+  then
+      (( VERBOSE  == TRUE )) && print -u 2 "# There are enough PPs to continue"
+  else
+      print -u 2 "There are not enough PPs to satisfy the request"
+      return 1
+  fi
 
 
 
@@ -247,133 +254,19 @@ function lvinfo {
   (( VERBOSE  == TRUE )) && print -u 2 "# LV_PP.............: ${LV_PP}"
   (( VERBOSE  == TRUE )) && print -u 2 "# LV_LP.............: ${LV_LP}"
   
-  LVINFO+=(["${FILESYSTEM}"]= 
-            (lv_fs_name="${FILESYSTEM}" 
-            lv_name="${LV_NAME}" 
-            lv_vg="${LV_VG}" 
-            lv_vg_pp_free="${LV_VG_PP_FREE}" 
-            lv_vg_pp_size="${LV_VG_PP_SIZE}" 
-            lv_lps="${LV_LP}" 
-            lv_pps="${LV_PP}" 
-            lv_mirrored="${LV_MIRRORED}" 
-            lv_snap_mb="${LV_SNAP_MB}")
+  LVINFO+=(
+            [lv_fs_name]="${FILESYSTEM}" 
+            [lv_name]="${LV_NAME}" 
+            [lv_vg]="${LV_VG}" 
+            [lv_vg_pp_free]="${LV_VG_PP_FREE}" 
+            [lv_vg_pp_size]="${LV_VG_PP_SIZE}" 
+            [lv_lps]="${LV_LP}" 
+            [lv_pps]="${LV_PP}" 
+            [lv_mirrored]="${LV_MIRRORED}" 
+            [lv_snap_mb]="${LV_SNAP_MB}"
         )
 
-  #(( SNAP_PP = ${LV_SNAP_MB} / ${VGINFO[1]} + 1 ))
-  #(( VERBOSE  == TRUE )) && print -u 2 "# SNAP_PP...........: ${SNAP_PP}"
-
-  #LVINFO+=( lv_snap_pp="${SNAP_PP}" vg_avail_pp="${VGINFO.pp_free}" )
-
   (( KORNOUT  == TRUE )) && print -- "${LVINFO[*]}"
-
-  trap "-" HUP
-
-  return 0
-}
-################################################################
-function usagemsg_vginfo {
-  print "
-Program: vginfo
-
-Place a brief description ( < 255 chars ) of your shell
-function here.
-
-Usage: ${1##*/} [-?vV] 
-
-  Where:
-    -v = Verbose mode - displays vginfo function info
-    -V = Very Verbose Mode - debug output displayed
-    -d = Output colon delimited records
-    -k = Output ksh93 array
-    -? = Help - display this message
-
-Author: Raymond L. Cox (rcox@unixandmore.com)
-
-\"AutoContent\" enabled
-"
-}
-################################################################
-#### 
-#### Description:
-#### 
-#### Initialize Volume Group Information
-#### 
-#### Assumptions:
-#### 
-#### None
-#### 
-#### Dependencies:
-#### 
-#### None
-#### 
-#### Products:
-#### 
-#### Array vginfo
-####    vginfo[vgname].pp_size
-####    vginfo[vgname].pp_free 
-####
-#### Configured Usage:
-#### 
-#### Describe how your shell function should be used.
-#### 
-#### Details:
-#### 
-#### Place nothing here, the details are your shell function.
-#### 
-################################################################
-function vginfo {
-  typeset VERSION="1.0"
-  typeset TRUE="0"
-  typeset FALSE="1"
-  typeset VERBOSE="${FALSE}"
-  typeset VERYVERB="${FALSE}"
-  typeset KORNOUT="${FALSE}"
-  typeset DATAOUT="${FALSE}"
-  typeset -L1 D=":"
-  #typeset -A VGINF
-
-#### Process the command line options and arguments.
-
-  while getopts ":vVg:" OPTION
-  do
-      case "${OPTION}" in
-          'v') VERBOSE="${TRUE}";;
-          'V') VERYVERB="${TRUE}";;
-          'd') DATAOUT="${TRUE}";;
-          'k') KORNOUT="${TRUE}";;
-          'g') VG="${OPTARG}";;
-          '?') vginfo "${0}" && return 1 ;;
-          ':') vginfo "${0}" && return 2 ;;
-          '#') NAME "${0}" && return 3 ;;
-      esac
-  done
-   
-  shift $(( ${OPTIND} - 1 ))
-  
-  trap "usagemsg_vginfo ${0}" EXIT
-
-  if (( KORNOUT == FALSE )) && (( DATAOUT == FALSE ))
-  then
-      KORNOUT="${TRUE}"
-  fi
-
-
-  trap "-" EXIT
-  
-  (( VERYVERB == TRUE )) && set -x
-  (( VERBOSE  == TRUE )) && print -u 2 "# Program...........: ${0}"
-  (( VERBOSE  == TRUE )) && print -u 2 "# Version...........: ${VERSION}"
-
-  pp_size=$(lsvg ${VG} | grep "PP SIZE" | awk 'BEGIN{FS=":"} {print$3}' | awk 'BEGIN{FS=" "} {print $1}')
-  pp_free=$(lsvg ${VG} | grep "FREE PP" | awk 'BEGIN{FS=":"} {print $3}'| awk 'BEGIN{FS=" "} {print $1}')
-  VGINF=(size="${pp_size}" free="${pp_free}")
-  #VGINF[size]="${pp_size}#"
-  #VGINF[free]="${pp_free}"
-
-  (( VERBOSE == TRUE )) && print -u 2 "# pp_size...........: ${VGINF.size}"
-  (( VERBOSE == TRUE )) && print -u 2 "# pp_free...........: ${VGINF.free}"
-
-  (( KORNOUT == TRUE )) && print -- "${VGINF[*]}"
 
   trap "-" HUP
 
